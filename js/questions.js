bc91125fb43ccfe7132f163b5dcb4fa3deaaca7b
HIND-HANSALI <hindhsl111@gmail.com>
2022-12-21 23:49:23 +0100

    adding questions js

---
+++ js/questions.js
@@ -0,0 +1,60 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Why is AWS more economical than traditional data centers for applications with varying compute workloads?",
+    answer: "Amazon EC2 instances can be launched on demand when needed",
+    options: [
+      "Amazon EC2 costs are billed on a monthly basis",
+      "Users retain full administrative access to their Amazon EC2 instances.",
+      "Amazon EC2 instances can be launched on demand when needed",
+      "Users can permanently run enough instances to handle peak workloads."
+    ]
+
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+ 
+];
