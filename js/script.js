1fa36f14cd37725e49b14a2bd2813bf0fea166c0
HIND-HANSALI <hindhsl111@gmail.com>
2022-12-23 15:06:09 +0100

    modifier timer fct

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 // stepper JS
-let question_count=0;
+let question_count;
 
 // const stepperItem = document.querySelector(".stepper__item");
 let step2 =document.querySelector(".step-2");
-
+let option_list = document.querySelector(".option_list");
+let option =document.querySelector(".option");
+let question_text = document.querySelector(".question_text");
 
 
 //getting all required elements
@@ -16,14 +18,11 @@ let quiz_box = document.querySelector(".quiz_box");
 let result_box = document.querySelector(".result_box");
 
 let timeCount=document.querySelector(".timer_sec");
-
-
+let timeLine=document.querySelector(".time_line");
 
 // if startQuiz button clicked
 
-function test(){
-    console.log('hhh');
-}
+
 // start_btn.onclick = ()=>{
 //     info_box.classList.add("activeInfo"); //show info box
 //     console.log('hind');
@@ -36,31 +35,32 @@ function test(){
 function Continue(){
     console.log('hhh');
    
-    // document.getElementById("myDIV").style.display = "none";
+    // document.getEleme ntById("myDIV").style.display = "none";
     info_box.style.display = "none";
     quiz_box.style.display = "block";
-    showQuetions(0);
+    randomQuestions = questions.sort(()=> Math.random() - .5);
+    question_count =0;
+    Next();
+
     counterQuestion(1);
     timerQuiz(15);
-    
+    timerLine(0);
     step2.classList.add('circle-acitve');
 
 }
 
 
 // getting questions and options from array
-function showQuetions(index){
-    let question_text = document.querySelector(".question_text");
-    let option_list = document.querySelector(".option_list");
+function showQuetions(questions){
     // counterQuestion(1);
 
-    let question_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
+    let question_tag = '<span>'+ questions.numb + ". " + questions.question +'</span>';
 
-    let option_tag='<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
-    + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
-    + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>'
-    + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
-
+    let option_tag='<div class="option"><span>'+ questions.options[0] +'</span></div>'
+    + '<div class="option"><span>'+ questions.options[1]+ '</span></div>'
+    + '<div class="option"><span>'+ questions.options[2] +'</span></div>'
+    + '<div class="option"><span>'+ questions.options[3] +'</span></div>';
+    
     question_text.innerHTML=question_tag ;
     option_list.innerHTML=option_tag;
     // let option_tag = '<div class="option"><span>'+ questions[0].options[0] +'</span></div>'
@@ -88,23 +88,32 @@ function showQuetions(index){
 // fct if next button clicked
 function Next(){
     console.log('nexxxt');
-    if(question_count < questions.length - 1){ //if question count is less than total question length
-        question_count++; //increment the question_count value
-        
-    showQuetions(question_count); //calling showQestions function
+    // if(question_count < questions.length - 1){ //if question count is less than total question length
+    //increment the question_count value
+   
+    showQuetions(randomQuestions[question_count]); //calling showQestions function
     counterQuestion(question_count+1);//passing numb of question value to counterQuestion
     clearInterval(counter);
     timerQuiz(15);
-    }else{
-        console.log("Questions completed");
+    // clearInterval(counterLine);
+    timerLine(0);
+    // clearquestion();
+    question_count++;
 
-    }
+    // }else{
+    //     console.log("Questions completed");
+    // }
 }
 
 
 
 
+// function clearquestion(){
 
+//     while(option.firstChild){
+//         option.removeChild(option.firstChild)
+//     }
+// }
 
 
 function counterQuestion(index){
@@ -121,6 +130,32 @@ function timerQuiz(time){
     counter=setInterval(timer,1000);
     function timer(){
         timeCount.textContent= time;
-        // time--;
+        time--;
+
+        if(time < 9){ //if timer is less than 9
+            let addZero = timeCount.textContent; 
+            timeCount.textContent = "0" + addZero; //add a 0 before time value
+        }
+        if (time<0){
+            clearInterval(counter);
+            timeCount.textContent="00";
+        }
+    }
+}
+let counterLine;
+function timerLine(time){
+
+    counterLine=setInterval(timerLinex,29);
+
+    function timerLinex(){
+        
+        time+=1;
+        let timelength = time+"px";
+        timeLine.style.width=timelength;
+        // console.log("hhh");
+        if (time>549){
+            // console.log("jgj");
+            clearInterval(counterLine);
+        }
     }
 }
