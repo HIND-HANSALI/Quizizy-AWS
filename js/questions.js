fac78efd3f92d552daa39fe738ed9cb43492db42
HIND-HANSALI <hindhsl111@gmail.com>
2022-12-26 21:51:50 +0100

    adding fct show result

--- js/questions.js
+++ js/questions.js
@@ -4,6 +4,7 @@ let questions = [
     numb: 1,
     question: "Why is AWS more economical than traditional data centers for applications with varying compute workloads?",
     answer: "Amazon EC2 instances can be launched on demand when needed",
+    justification:" C –The ability to launch instances on demand when needed allows users to launch and terminate instances in response to a varying workload. This is a more economical practice than purchasing enough on-premises servers to handle the peak load.",
     options: [
       "Amazon EC2 costs are billed on a monthly basis",
       "Users retain full administrative access to their Amazon EC2 instances.",
@@ -16,6 +17,7 @@ let questions = [
     numb: 2,
     question: "Which AWS service would simplify the migration of a database to AWS?",
     answer: "AWS Database Migration Service (AWS DMS)",
+    justification:" B – AWS DMS helps users migrate databases to AWS quickly and securely. The source database remains fully operational during the migration, minimizing downtime to applications that rely on the database. AWS DMS can migrate data to and from most widely used commercial and open-source databases.",
     options: [
       "AWS Storage Gateway",
       "AWS Database Migration Service (AWS DMS)",
@@ -27,6 +29,7 @@ let questions = [
     numb: 3,
     question: "Which AWS offering enables users to find, buy, and immediately start using software solutions in their AWS environment?",
     answer: "AWS Marketplace",
+    justification:"D – AWS Marketplace is a digital catalog with thousands of software listings from independent software vendors that makes it easy to find, test, buy, and deploy software that runs on AWS.",    
     options: [
       "AWS Config",
       "AWS OpsWorks",
@@ -38,6 +41,7 @@ let questions = [
     numb: 4,
     question: "Which AWS networking service enables a company to create a virtual network within AWS?",
     answer: "Amazon Virtual Private Cloud (Amazon VPC)",
+    justification:"",
     options: [
       "AWS Config",
       "Amazon Route 53",
@@ -49,6 +53,7 @@ let questions = [
     numb: 5,
     question: "Which of the following is an AWS responsibility under the AWS shared responsibility model?",
     answer: "Maintaining physical hardware",
+    justification:"",
     options: [
       "Configuring third-party applications",
       "Maintaining physical hardware",
@@ -56,16 +61,66 @@ let questions = [
       "Managing guest operating systems"
     ]
   },
-  // {
-  //   numb: 6,
-  //   question: "Which of the following is an AWS responsibility under the AWS shared responsibility model?",
-  //   answer: "Maintaining physical hardware",
-  //   options: [
-  //     "Configuring third-party applications",
-  //     "Maintaining physical hardware",
-  //     "Securing application access and data",
-  //     "Managing guest operating systems"
-  //   ]
-  // },
+  {
+    numb: 6,
+    question: "Which component of the AWS global infrastructure does Amazon CloudFront use to ensure low-latency delivery?",
+    answer: "Edge locations",
+    justification:"",
+    options: [
+      "AWS Regions",
+      "Edge locations",
+      "Availability Zones",
+      "Virtual Private Cloud (VPC)"
+    ]
+  },
+  {
+    numb: 7,
+    question: "How would a system administrator add an additional layer of login security to a user's AWS Management Console?",
+    answer: "Enable multi-factor authentication",
+    justification:"",
+    options: [
+      "Use Amazon Cloud Directory",
+      "Audit AWS Identity and Access Management (IAM) roles",
+      "Enable multi-factor authentication",
+      "Enable AWS CloudTrail"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which service can identify the user that made the API call when an Amazon EC2 instance is terminated?Which service can identify the user that made the API call when an Amazon EC2 instance is terminated?",
+    answer: "AWS CloudTrail",
+    justification:"",
+    options: [
+      "AWS Trusted Advisor",
+      "AWS CloudTrail",
+      "AWS X-Ray",
+      "AWS Identity and Access Management (AWS IAM)"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which service would be used to send alerts based on Amazon CloudWatch alarms?",
+    answer: "Amazon Simple Notification Service (Amazon SNS)",
+    justification:"",
+    options: [
+      "Amazon Simple Notification Service (Amazon SNS)",
+      "AWS CloudTrail",
+      "AWS Trusted Advisor",
+      "Amazon Route 53"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Where can a user find information about prohibited actions on the AWS infrastructure?",
+    answer: "AWS Acceptable Use Policy",
+    justification:"",
+    options: [
+      "AWS Trusted Advisor",
+      "Maintaining physical hardware",
+      "AWS Billing Console",
+      "AWS Acceptable Use Policy"
+    ]
+  },
+
  
 ];
--- js/script.js
+++ js/script.js
@@ -2,10 +2,13 @@
 let question_count;
 
 // const stepperItem = document.querySelector(".stepper__item");
-let step2 =document.querySelector(".step-2");
+let step2 = document.querySelector(".step-2");
+let step3 = document.querySelector(".step-3");
 let option_list = document.querySelector(".option_list");
-let option =document.querySelector(".option");
+let option = document.querySelector(".option");
 let question_text = document.querySelector(".question_text");
+
+let next_btn= document.querySelector(".next_bt");
 
 
 //getting all required elements
@@ -16,11 +19,13 @@ let info_box = document.querySelector(".info_box");
 // const continue_btn = info_box.querySelector(".buttons .continue");
 let quiz_box = document.querySelector(".quiz_box");
 let result_box = document.querySelector(".result_box");
+let restart_btn=document.querySelector(".restart");
+let quit_btn=document.querySelector(".quit");
 
-let timeCount=document.querySelector(".timer_sec");
-let timeLine=document.querySelector(".time_line");
+let timeCount = document.querySelector(".timer_sec");
+let timeLine = document.querySelector(".time_line");
 
-let time_Pogress=document.querySelector(".time_Pogress");
+let time_Pogress = document.querySelector(".time_Pogress");
 
 // if startQuiz button clicked
 
@@ -34,16 +39,17 @@ let time_Pogress=document.querySelector(".time_Pogress");
 // continue_btn.onclick = ()=>{
 //     info_box.classList.add("activeInfo");
 // }
-function Continue(){
+function Continue() {
     console.log('hhh');
-   
+
     // document.getEleme ntById("myDIV").style.display = "none";
     info_box.style.display = "none";
     quiz_box.style.display = "block";
-    randomQuestions = questions.sort(()=> Math.random() - .5);
-    question_count =0;
+    randomQuestions = questions.sort(() => Math.random() - .5);
+    question_count = 0;
     Next();
-
+    clearInterval(counter);
+    clearInterval(counterLine);
     counterQuestion(1);
     timerQuiz(15);
     timerLine(0);
@@ -53,76 +59,92 @@ function Continue(){
 
 
 // getting questions and options from array
-function showQuetions(questions){
+function showQuetions(questions) {
     // counterQuestion(1);
 
-    let question_tag = '<span>'+ questions.numb + ". " + questions.question +'</span>';
+    let question_tag = '<span>' + questions.numb + ". " + questions.question + '</span>';
 
-    let option_tag='<div class="option"><span>'+ questions.options[0] +'</span></div>'
-    + '<div class="option"><span>'+ questions.options[1]+ '</span></div>'
-    + '<div class="option"><span>'+ questions.options[2] +'</span></div>'
-    + '<div class="option"><span>'+ questions.options[3] +'</span></div>';
-    
-    question_text.innerHTML=question_tag ;
-    option_list.innerHTML=option_tag;
+    let option_tag = '<div class="option"><span>' + questions.options[0] + '</span></div>'
+        + '<div class="option"><span>' + questions.options[1] + '</span></div>'
+        + '<div class="option"><span>' + questions.options[2] + '</span></div>'
+        + '<div class="option"><span>' + questions.options[3] + '</span></div>';
+
+    question_text.innerHTML = question_tag;
+    option_list.innerHTML = option_tag;
     // let option_tag = '<div class="option"><span>'+ questions[0].options[0] +'</span></div>'
 
-//    proooob
-    let option=option_list.querySelectorAll(".option");
+    //    proooob
+    let option = option_list.querySelectorAll(".option");
 
 
     // set onclick attribute to all options
-    for(let i=0; i < option.length; i++){
-        option[i].setAttribute("onclick", "optionSelected(this)");
+    for (let i = 0; i < option.length; i++) {
+        option[i].setAttribute("onclick", "optionSelected(this," + questions.numb + ")");
     }
-
+    
 
 }
+let userScore=0;
 // if user clicked an option
-function optionSelected(answer){
+function optionSelected(answer, numb) {
     // console.log("hiii selected option");
     let userAnswer = answer.textContent;
     // console.log(userAnswer );
-
-    let correctAnswer = questions[question_count].answer;//correct answer from array
-    let allOptions=option_list.children.length;
-    console.log(correctAnswer);
+    questions.forEach(element => {
+        // console.log(question_count +"test");
+        if (element.numb == numb) {
+            let correctAnswer = element['answer'];//correct answer from array
+            // let allOptions=option_list.children.length;
+            console.log(correctAnswer);
+            if (userAnswer == correctAnswer) {
+                userScore++;
+                console.log(userScore);
+                answer.classList.add("correct");
+            } else {
+                answer.classList.add("incorrect");
+            }
+            //once user select an option  disabled all options
+            // for (i = 0; i < allOptions; i++) {
+            //     // option_list.children[i].classList.add("disabled"); 
+            // }
+            next_btn.style.display="block";
+            
+        }
+    });
     // answer.classList.add("correct");
-    if(userAnswer==correctAnswer){
-        answer.classList.add("correct");
-    }else{
-        answer.classList.add("incorrect");
-    }
-    //once user select an option  disabled all options
-    for(i=0; i < allOptions; i++){
-        // option_list.children[i].classList.add("disabled"); 
-    }
+
 
 
 }
 
 // fct if next button clicked
 var progress = 0;
-function Next(){
-    progress += 10 ;
+function Next() {
+    clearInterval(counter);
+    progress += 10;
     console.log('nexxxt');
-    // if(question_count < questions.length - 1){ //if question count is less than total question length
+    if(question_count < questions.length ){ //if question count is less than total question length
     //increment the question_count value
-    time_Pogress.style.width=progress+"%";
+    time_Pogress.style.width = progress + "%";
 
     showQuetions(randomQuestions[question_count]); //calling showQestions function
-    counterQuestion(question_count+1);//passing numb of question value to counterQuestion
+    counterQuestion(question_count + 1);//passing numb of question value to counterQuestion
     clearInterval(counter);
+    
     timerQuiz(15);
-    // clearInterval(counterLine);
+    clearInterval(counterLine);
     timerLine(0);
     // clearquestion();
     question_count++;
     
+    
 
-    // }else{
-    //     console.log("Questions completed");
-    // }
+
+    }else{
+        console.log("Questions completed");
+        showResultBox();
+        step3.classList.add('circle-acitve');
+    }
 }
 
 // function clearquestion(){
@@ -133,47 +155,77 @@ function Next(){
 // }
 
 
-function counterQuestion(index){
+function counterQuestion(index) {
     //creating a new span tag and passing the question number and total question
     const total_ques_counter = document.querySelector(".total_question");
-   
-    let totalQueCounTag = '<p><strong>'+ index +' of '+ questions.length +' </strong> Questions</p>';
+
+    let totalQueCounTag = '<p><strong>' + index + ' of ' + questions.length + ' </strong> Questions</p>';
 
 
     total_ques_counter.innerHTML = totalQueCounTag;  //adding new span tag inside bottom_ques_counter
 }
-let counter;
-function timerQuiz(time){
-    counter=setInterval(timer,1000);
-    function timer(){
-        timeCount.textContent= time;
-        // time--;
 
-        if(time < 9){ //if timer is less than 9
-            let addZero = timeCount.textContent; 
+function showResultBox(){
+    quiz_box.style.display="none";
+    result_box.style.display="block";
+    result_box.style.opacity="1";
+    let score_text=document.querySelector(".score_text");
+    // score_text.innerHTML="userScore";
+    if(userScore>7){
+        let scoreTag='<span>Congrats <p>'+userScore+'</p>out of <p>'+questions.length+'</p></span>';
+        score_text.innerHTML=scoreTag;
+    }
+    else if(userScore>5){
+        let scoreTag='<span>Good <p>'+userScore+'</p>out of <p>'+questions.length+'</p></span>';
+        score_text.innerHTML=scoreTag;
+    }
+    else{
+        let scoreTag='<span>Sorry,you got only <p>'+userScore+'</p>out of <p>'+questions.length+'</p></span>';
+        score_text.innerHTML=scoreTag;
+    }
+
+}
+function Restart(){
+    result_box.style.display="none";
+    quiz_box.style.display="block";
+
+}
+// function Justification(){
+
+
+// }
+let counter;
+function timerQuiz(time) {
+    counter = setInterval(timer, 1000);
+    function timer() {
+        timeCount.textContent = time;
+        time--;
+
+        if (time < 9) { //if timer is less than 9
+            let addZero = timeCount.textContent;
             timeCount.textContent = "0" + addZero; //add a 0 before time value
         }
-        if (time<0){
+        if (time < 0) {
             clearInterval(counter);
-            timeCount.textContent="00";
+            timeCount.textContent = "00";
         }
     }
 }
 let counterLine;
-function timerLine(time){
+function timerLine(time) {
 
-    counterLine=setInterval(timerLinex,29);
+    counterLine = setInterval(timerLinex, 29);
 
-    function timerLinex(){
-        
+    function timerLinex() {
+
         //  time+=1;
-         if(time<540){
-            let timelength = time+"px";
-        timeLine.style.width=timelength;
-        
-        // time++;
-         }
-        if(time>540){
+        if (time < 540) {
+            let timelength = time + "px";
+            timeLine.style.width = timelength;
+
+            // time++;
+        }
+        if (time > 540) {
             clearInterval(counterLine);
         }
     }
